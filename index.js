104b499a2844b3d7ecb40aa397d21e710d6e44d8
Chloe Wintzer <chloe.wintzer@rearc.io>
2021-12-13 22:39:42 -0800

    tunnel support

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Webserver = require('./lib/Webserver')
 const SecureProxy = require('./lib/proxies/SecureProxy')
 const InsecureProxy = require('./lib/proxies/InsecureProxy')
+const Tunnel = require('./lib/Tunnel')
 
 module.exports = {
   Webserver,
   SecureProxy,
-  InsecureProxy
+  InsecureProxy,
+  Tunnel
 }
